6d0f86bd7a55b94843338e70b018923ae2e9c798
xiaoranran <xiaoranran1993@outlook.com>
2022-03-21 19:18:02 +0800

    feat(master): test tree view

--- test/tree_view_test.dart
+++ test/tree_view_test.dart
@@ -1,19 +1,48 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:flutter_tree/flutter_tree.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_tree/flutter_tree.dart';
 
-// import '../example/tree_data.dart';
+MaterialApp myApp = MaterialApp(
+  home: Scaffold(
+    body: TreeView(
+      showFilter: true,
+      showActions: true,
+      showCheckBox: true,
+      data: [
+        TreeNodeData(
+          title: 'title',
+          expaned: false,
+          checked: false,
+          children: [],
+          extra: null,
+        ),
+      ],
+    ),
+  ),
+);
 
-// MaterialApp myApp = MaterialApp(
-//   home: Scaffold(
-//     body: TreeView(data: treeData),
-//   ),
-// );
+void main() {
+  testWidgets('Tree view render', (WidgetTester tester) async {
+    await tester.pumpWidget(myApp);
 
-// void main() {
-//   testWidgets('Tree view render', (WidgetTester tester) async {
-//     await tester.pumpWidget(myApp);
+    expect(find.byType(TreeView), findsWidgets);
+  });
 
-//     expect(find.byType(Icon), findsWidgets);
-//   });
-// }
+  testWidgets('Render filter', (WidgetTester tester) async {
+    await tester.pumpWidget(myApp);
+
+    expect(find.byType(TextField), findsWidgets);
+  });
+
+  testWidgets('Render checkbox', (WidgetTester tester) async {
+    await tester.pumpWidget(myApp);
+
+    expect(find.byType(Checkbox), findsWidgets);
+  });
+
+  testWidgets('Render actions', (WidgetTester tester) async {
+    await tester.pumpWidget(myApp);
+
+    expect(find.byType(TextButton), findsWidgets);
+  });
+}
